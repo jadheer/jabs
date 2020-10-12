77c74d5e2c698d8940b85ca06fff962bc99a93fc
jadheer <ajadheer@gmail.com>
2020-10-12 11:52:13 +0530

    otp done

--- app/Models/OpportunityDetail.php
+++ app/Models/OpportunityDetail.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class OpportunityDetail extends Model
 {
+
+    protected $casts = [
+      'start_date' => 'datetime',
+      'end_date' => 'datetime',
+    ];
+
+
     public function opportunity(){
       return $this->belongsTo(Opportunity::class);
     }
