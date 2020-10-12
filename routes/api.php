399dca0d213c5c72d9e1475a6ec6bc5dded4feef
jadheer <ajadheer@gmail.com>
2020-10-12 12:11:23 +0530

    otp done

--- routes/api.php
+++ routes/api.php
@@ -14,23 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::prefix('auth')->group(function(){
-  //api/auth/register
-  Route::post('/register','AuthController@register');
-  Route::post('/login','AuthController@login');
-  Route::get('/logout','AuthController@logout')->middleware('auth:api');
-  Route::get('/user','AuthController@user')->middleware('auth:api');
-  Route::get('/authentication-failed','AuthController@authFailed')->name('auth-failed');
+    //api/auth/register
+    Route::post('/register', 'AuthController@register');
+    Route::post('/login', 'AuthController@login');
+    Route::get('/logout', 'AuthController@logout')->middleware('auth:api');
+    Route::get('/user', 'AuthController@user')->middleware('auth:api');
+    Route::get('authentication-failed', 'AuthController@authFailed')->name('auth-failed');
+
 });
 
-Route::group(['prefix'=>'lookups'], function(){
-// Route::group(['prefix'=>'lookups','middleware'=>'auth:api'], function(){
-    Route::resource('opportunity','OpportunityController');
+
+
+Route::group(['prefix' =>'lookups', 'middleware' => 'auth:api'], function (){
     Route::resource('category','CategoryController');
     Route::resource('country','CountryController');
 });
 
+Route::group(['middleware' => 'auth:api'], function (){
+
+    //Opportunities
+   Route::resource('opportunity','OpportunityController');
+
+    // Questions
+    Route::get('questions','QuestionController@index');
+    Route::post('question','QuestionController@store');
+    Route::put('question/{question}','QuestionController@update');
+
+    // Favorites
+   Route::get('favorites','FavoriteController@index');
+   Route::post('favorite','FavoriteController@store');
+   Route::put('favorite/{favorite}','FavoriteController@update');
+
+   // ToDo: Upload Images for Opportunities and Forum
+});
